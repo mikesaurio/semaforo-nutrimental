5de9719c368fba3dcdc4342bb274a20da2728512
mikesaurio <mikesaurio@gmail.com>
2016-06-12 15:15:40 -0500

    se crea todo para nuevo apk

--- library/build/generated/source/buildConfig/release/com/akexorcist/roundcornerprogressbar/BuildConfig.java
+++ library/build/generated/source/buildConfig/release/com/akexorcist/roundcornerprogressbar/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.akexorcist.roundcornerprogressbar";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 166;
-  public static final String VERSION_NAME = "1.6.6";
+  public static final int VERSION_CODE = 167;
+  public static final String VERSION_NAME = "1.6.7";
 }
