c836ce8b567f65fdf274d81c1f029bcb634be2ad
mikesaurio <mikesaurio@gmail.com>
2017-06-14 21:03:19 -0500

    se modifica la vista de sellos

--- app/src/main/java/curisteando/com/semaforonutrimental/activities/ResultadosNutricionActivity.java
+++ app/src/main/java/curisteando/com/semaforonutrimental/activities/ResultadosNutricionActivity.java
@@ -30,18 +30,83 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import curisteando.com.barcodez.SimpleScannerFragmentActivity;
 import curisteando.com.semaforonutrimental.R;
+import curisteando.com.semaforonutrimental.beans.DatosBean;
 import curisteando.com.semaforonutrimental.utilidades.Constantes;
 import curisteando.com.semaforonutrimental.utilidades.TipoResultado;
 import curisteando.com.semaforonutrimental.utilidades.Utils;
 
 
-public class ResultadosNutricionActivity extends AppCompatActivity implements View.OnClickListener {
+public class ResultadosNutricionActivity extends AppCompatActivity  {
 
 
     @Override
-    public void onClick(View v) {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resultados_nutricion);
+
+        restoreActionBar();
+
+        Intent i = getIntent();
+        Bundle extras = i.getExtras();
+       String  json_string = extras.getString(Constantes.CONS_RESPONSE);
+
+        init(json_string);
+    }
+
+    public void init(String json_string){
+        try {
+            JSONObject jsonObjectFeed = new JSONObject(json_string);
+            JSONObject jsonObject= new JSONObject(jsonObjectFeed.getString("feed"));
+            DatosBean db = new DatosBean();
+            db.setType_product(jsonObject.getString("type"));
+            db.setCode(jsonObject.getString("code"));
+            db.setCategory(jsonObject.getString("category"));
+            db.setSubcategory(jsonObject.getString("subcategory"));
+            db.setName(jsonObject.getString("name"));
+
+            db.setMark(jsonObject.getString("mark"));
+            db.setCompany(jsonObject.getString("company"));
+            db.setNet_content(jsonObject.getString("net_content"));
+            db.setPortion(jsonObject.getString("portion"));
+            db.setEnergy(jsonObject.getString("energy"));
+
+            db.setSugar_portion(jsonObject.getString("sugar_portion"));
+            db.setGs_portion(jsonObject.getString("gs_portion"));
+            db.setSodio_portion(jsonObject.getString("sodio_portion"));
+            db.setSweetener(jsonObject.getString("sweetener"));
+            db.setCaloria_100(jsonObject.getString("caloria_100"));
+
+            db.setSugar_100(jsonObject.getString("sugar_100"));
+            db.setGs_100(jsonObject.getString("gs_100"));
+            db.setSodio_100(jsonObject.getString("sodio_100"));
+            db.setResult_caloria(jsonObject.getBoolean("result_caloria"));
+            db.setResult_sugar(jsonObject.getBoolean("result_sugar"));
+
+            db.setResult_gs(jsonObject.getBoolean("result_gs"));
+            db.setResult_sodio(jsonObject.getBoolean("result_sodio"));
+            db.setTotal_true(jsonObject.getString("total_true"));
+            db.setMessage(jsonObject.getString("message"));
+            db.setRecommendation(jsonObject.getString("recommendation"));
+
+            make_sellos(db);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void make_sellos(DatosBean db){
 
     }
+
+    public void restoreActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        actionBar = Utils.getFormatActionBar(this, actionBar);
+        actionBar.setHomeButtonEnabled(true);
+    }
+
 }
