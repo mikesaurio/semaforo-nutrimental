e5e2f0b5f3ed0180390f34e089199e9fc0dcb877
mikesaurio <mikesaurio@gmail.com>
2017-06-17 12:50:37 -0500

    se generan los sellos por productos

--- app/src/main/java/curisteando/com/semaforonutrimental/activities/ResultadosNutricionActivity.java
+++ app/src/main/java/curisteando/com/semaforonutrimental/activities/ResultadosNutricionActivity.java
@@ -1,43 +1,18 @@
 package curisteando.com.semaforonutrimental.activities;
 
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.graphics.Color;
-import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Html;
-import android.text.method.LinkMovementMethod;
-import android.util.DisplayMetrics;
-import android.util.Log;
-import android.view.Gravity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
-
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import curisteando.com.barcodez.SimpleScannerFragmentActivity;
 import curisteando.com.semaforonutrimental.R;
 import curisteando.com.semaforonutrimental.beans.DatosBean;
 import curisteando.com.semaforonutrimental.utilidades.Constantes;
-import curisteando.com.semaforonutrimental.utilidades.TipoResultado;
 import curisteando.com.semaforonutrimental.utilidades.Utils;
 
 
@@ -53,7 +28,7 @@ public class ResultadosNutricionActivity extends AppCompatActivity  {
 
         Intent i = getIntent();
         Bundle extras = i.getExtras();
-       String  json_string = extras.getString(Constantes.CONS_RESPONSE);
+        String  json_string = extras.getString(Constantes.CONS_RESPONSE);
 
         init(json_string);
     }
@@ -100,7 +75,111 @@ public class ResultadosNutricionActivity extends AppCompatActivity  {
     }
 
     public void make_sellos(DatosBean db){
+        LinearLayout ll = null;
+        if(db.getTotal_true().equals("1")){
+            ll = (LinearLayout)findViewById(R.id.sello_one);
+            set_sellos(0,ll,db.isResult_caloria(),db.isResult_sugar(),db.isResult_gs(), db.isResult_sodio());
+        }else if(db.getTotal_true().equals("1")){
+            ll = (LinearLayout)findViewById(R.id.sello_one);
+            set_sellos(1,ll,db.isResult_caloria(),db.isResult_sugar(),db.isResult_gs(), db.isResult_sodio());
+        }else  if(db.getTotal_true().equals("2")){
+            ll = (LinearLayout)findViewById(R.id.sello_two);
+            set_sellos(2,ll,db.isResult_caloria(),db.isResult_sugar(),db.isResult_gs(), db.isResult_sodio());
+        }else  if(db.getTotal_true().equals("3")){
+            ll = (LinearLayout)findViewById(R.id.sello_three);
+            set_sellos(3,ll,db.isResult_caloria(),db.isResult_sugar(),db.isResult_gs(), db.isResult_sodio());
+        }else if(db.getTotal_true().equals("4")){
+            ll = (LinearLayout)findViewById(R.id.sello_four);
+            set_sellos(4,ll,db.isResult_caloria(),db.isResult_sugar(),db.isResult_gs(), db.isResult_sodio());
+        }
+        ll.setVisibility(View.VISIBLE);
+    }
 
+    public void set_sellos(int num_sellos, LinearLayout ll, boolean calorias, boolean sugar, boolean gs, boolean sodio){
+        if(num_sellos==0){
+            change_image(((ImageView) ll.findViewById(R.id.iv_sello_one)), R.drawable.ic_launch_ok);
+        }
+        if(num_sellos==1){
+            if(calorias){
+                change_image(((ImageView) ll.findViewById(R.id.iv_sello_one)), R.drawable.ic_launch_calorias);
+            }else if(sugar){
+                change_image(((ImageView) ll.findViewById(R.id.iv_sello_one)), R.drawable.ic_launch_azucar);
+            }else if(gs){
+                change_image(((ImageView) ll.findViewById(R.id.iv_sello_one)), R.drawable.ic_launch_grasas);
+            }else if(sodio){
+                change_image(((ImageView) ll.findViewById(R.id.iv_sello_one)), R.drawable.ic_launch_sodio);
+            }
+        }else if(num_sellos==2){
+            if(calorias && sugar){
+                change_image(((ImageView) ll.findViewById(R.id.iv_sello_one)),((ImageView) ll.findViewById(R.id.iv_sello_two)), R.drawable.ic_launch_calorias, R.drawable.ic_launch_azucar);
+            }else if(calorias && gs){
+                change_image(((ImageView) ll.findViewById(R.id.iv_sello_one)),((ImageView) ll.findViewById(R.id.iv_sello_two)), R.drawable.ic_launch_calorias, R.drawable.ic_launch_grasas);
+            }else if(calorias && sodio){
+                change_image(((ImageView) ll.findViewById(R.id.iv_sello_one)),((ImageView) ll.findViewById(R.id.iv_sello_two)), R.drawable.ic_launch_calorias, R.drawable.ic_launch_sodio);
+            }else if(sugar && gs){
+                change_image(((ImageView) ll.findViewById(R.id.iv_sello_one)),((ImageView) ll.findViewById(R.id.iv_sello_two)), R.drawable.ic_launch_azucar, R.drawable.ic_launch_grasas);
+            }else if(sugar && sodio){
+                change_image(((ImageView) ll.findViewById(R.id.iv_sello_one)),((ImageView) ll.findViewById(R.id.iv_sello_two)), R.drawable.ic_launch_azucar, R.drawable.ic_launch_sodio);
+            }else if(gs && sodio){
+                change_image(((ImageView) ll.findViewById(R.id.iv_sello_one)),((ImageView) ll.findViewById(R.id.iv_sello_two)), R.drawable.ic_launch_grasas, R.drawable.ic_launch_sodio);
+            }
+        }else if(num_sellos==3){
+            if(calorias && sugar && gs){
+                change_image(((ImageView) ll.findViewById(R.id.iv_sello_one)),((ImageView) ll.findViewById(R.id.iv_sello_two)),((ImageView) ll.findViewById(R.id.iv_sello_three)), R.drawable.ic_launch_calorias, R.drawable.ic_launch_azucar, R.drawable.ic_launch_grasas);
+            }else if(calorias && sugar && sodio){
+                change_image(((ImageView) ll.findViewById(R.id.iv_sello_one)),((ImageView) ll.findViewById(R.id.iv_sello_two)),((ImageView) ll.findViewById(R.id.iv_sello_three)), R.drawable.ic_launch_calorias, R.drawable.ic_launch_azucar, R.drawable.ic_launch_sodio);
+            }else if(calorias && gs && sodio){
+                change_image(((ImageView) ll.findViewById(R.id.iv_sello_one)),((ImageView) ll.findViewById(R.id.iv_sello_two)),((ImageView) ll.findViewById(R.id.iv_sello_three)), R.drawable.ic_launch_calorias, R.drawable.ic_launch_grasas, R.drawable.ic_launch_sodio);
+            }else if(sugar && gs && sodio){
+                change_image(((ImageView) ll.findViewById(R.id.iv_sello_one)),((ImageView) ll.findViewById(R.id.iv_sello_two)),((ImageView) ll.findViewById(R.id.iv_sello_three)), R.drawable.ic_launch_grasas, R.drawable.ic_launch_azucar, R.drawable.ic_launch_sodio);
+            }
+        }else if(num_sellos==4){
+            change_image(((ImageView) ll.findViewById(R.id.iv_sello_one)),((ImageView) ll.findViewById(R.id.iv_sello_two)),((ImageView) ll.findViewById(R.id.iv_sello_three)),((ImageView) ll.findViewById(R.id.iv_sello_four)), R.drawable.ic_launch_calorias, R.drawable.ic_launch_azucar, R.drawable.ic_launch_grasas, R.drawable.ic_launch_sodio);
+        }
+    }
+
+    public void change_image(ImageView iv, int img){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            iv.setImageDrawable(getResources().getDrawable(img, getApplicationContext().getTheme()));
+        } else {
+            iv.setImageDrawable(getResources().getDrawable(img));
+        }
+    }
+
+    public void change_image(ImageView iv,ImageView iv2, int img, int img2){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            iv.setImageDrawable(getResources().getDrawable(img, getApplicationContext().getTheme()));
+            iv2.setImageDrawable(getResources().getDrawable(img2, getApplicationContext().getTheme()));
+        } else {
+            iv.setImageDrawable(getResources().getDrawable(img));
+            iv2.setImageDrawable(getResources().getDrawable(img2));
+        }
+    }
+
+    public void change_image(ImageView iv,ImageView iv2,ImageView iv3, int img, int img2,int img3){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            iv.setImageDrawable(getResources().getDrawable(img, getApplicationContext().getTheme()));
+            iv2.setImageDrawable(getResources().getDrawable(img2, getApplicationContext().getTheme()));
+            iv3.setImageDrawable(getResources().getDrawable(img3, getApplicationContext().getTheme()));
+        } else {
+            iv.setImageDrawable(getResources().getDrawable(img));
+            iv2.setImageDrawable(getResources().getDrawable(img2));
+            iv3.setImageDrawable(getResources().getDrawable(img3));
+        }
+    }
+
+    public void change_image(ImageView iv,ImageView iv2,ImageView iv3,ImageView iv4, int img, int img2,int img3,int img4){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            iv.setImageDrawable(getResources().getDrawable(img, getApplicationContext().getTheme()));
+            iv2.setImageDrawable(getResources().getDrawable(img2, getApplicationContext().getTheme()));
+            iv3.setImageDrawable(getResources().getDrawable(img3, getApplicationContext().getTheme()));
+            iv4.setImageDrawable(getResources().getDrawable(img4, getApplicationContext().getTheme()));
+        } else {
+            iv.setImageDrawable(getResources().getDrawable(img));
+            iv2.setImageDrawable(getResources().getDrawable(img2));
+            iv3.setImageDrawable(getResources().getDrawable(img3));
+            iv4.setImageDrawable(getResources().getDrawable(img4));
+        }
     }
 
     public void restoreActionBar() {
